077088e2ac2f9f63b22e9b697a586cfac41e2fd5
Bruno Lorenzo <bruno.lorenzop@gmail.com>
2021-10-15 22:19:55 -0300

    Initial commit

---
+++ KeychainManager.swift
@@ -0,0 +1,181 @@
+import Foundation
+
+final class KeychainManager {
+    
+    static let shared = KeychainManager()
+    
+    typealias KeychainDictionary = [String : Any]
+    typealias ItemAttributes = [CFString : Any]
+    
+    func saveItem<T: Encodable>(_ item: T, itemClass: ItemClass, attributes: ItemAttributes? = nil) throws {
+        
+        let itemData = try JSONEncoder().encode(item)
+        var query: KeychainDictionary = [
+            kSecClass as String: itemClass.rawValue,
+            kSecValueData as String: itemData as AnyObject
+        ]
+        
+        if let itemAttributes = attributes {
+            query.addAttributes(itemAttributes)
+        }
+        
+        let result = SecItemAdd(query as CFDictionary, nil)
+        if result != errSecSuccess {
+            throw convertError(result)
+        }
+    }
+    
+    func retrieveItem<T: Decodable>(ofClass itemClass: ItemClass, attributes: ItemAttributes? = nil) throws -> T {
+        var query: KeychainDictionary = [
+            kSecClass as String: itemClass.rawValue,
+            kSecReturnAttributes as String: true,
+            kSecReturnData as String: true
+        ]
+        
+        if let itemAttributes = attributes {
+            query.addAttributes(itemAttributes)
+        }
+        
+        var item: CFTypeRef?
+        let result = SecItemCopyMatching(query as CFDictionary, &item)
+        if result != errSecSuccess {
+            throw convertError(result)
+        }
+        
+        guard let keychainItem = item as? [String : Any], let data = keychainItem[kSecValueData as String] as? Data else {
+            throw KeychainError.invalidData
+        }
+        
+        return try JSONDecoder().decode(T.self, from: data)
+    }
+    
+    func updateItem<T: Encodable>(with item: T, ofClass itemClass: ItemClass, attributes: ItemAttributes? = nil) throws {
+        var query: KeychainDictionary = [
+            kSecClass as String: itemClass.rawValue
+        ]
+        
+        if let itemAttributes = attributes {
+            query.addAttributes(itemAttributes)
+        }
+        
+        let itemData = try JSONEncoder().encode(item)
+        
+        let attributesToUpdate: KeychainDictionary = [
+            kSecValueData as String: itemData as AnyObject
+        ]
+        
+        let result = SecItemUpdate(query as CFDictionary, attributesToUpdate as CFDictionary)
+        if result != errSecSuccess {
+            throw convertError(result)
+        }
+    }
+    
+    func deleteImte(ofClass itemClass: ItemClass, attributes: ItemAttributes) throws {
+        var query: KeychainDictionary = [
+            kSecClass as String: itemClass.rawValue
+        ]
+        
+        query.addAttributes(attributes)
+        
+        let result = SecItemDelete(query as CFDictionary)
+        if result != errSecSuccess {
+            throw convertError(result)
+        }
+    }
+}
+
+// MARK: - ItemClass
+
+extension KeychainManager {
+    enum ItemClass: RawRepresentable {
+        typealias RawValue = CFString
+        
+        case generic
+        case password
+        case certificate
+        case cryptography
+        case identity
+        
+        init?(rawValue: CFString) {
+            switch rawValue {
+            case kSecClassGenericPassword:
+                self = .generic
+            case kSecClassInternetPassword:
+                self = .password
+            case kSecClassCertificate:
+                self = .certificate
+            case kSecClassKey:
+                self = .cryptography
+            case kSecClassIdentity:
+                self = .identity
+            default:
+                return nil
+            }
+        }
+        
+        var rawValue: CFString {
+            switch self {
+            case .generic:
+                return kSecClassGenericPassword
+            case .password:
+                return kSecClassInternetPassword
+            case .certificate:
+                return kSecClassCertificate
+            case .cryptography:
+                return kSecClassKey
+            case .identity:
+                return kSecClassIdentity
+            }
+        }
+    }
+}
+
+// MARK: - Errors
+
+extension KeychainManager {
+    enum KeychainError: Error {
+        case invalidData
+        case itemNotFound
+        case duplicateItem
+        case incorrectAttributeForClass
+        case unexpected(OSStatus)
+        
+        var localizedDescription: String {
+            switch self {
+            case .invalidData:
+                return "Invalid data"
+            case .itemNotFound:
+                return "Item not found"
+            case .duplicateItem:
+                return "Duplicate Item"
+            case .incorrectAttributeForClass:
+                return "Incorrect Attribute for Class"
+            case .unexpected(let oSStatus):
+                return "Unexpected error - \(oSStatus)"
+            }
+        }
+    }
+    
+    private func convertError(_ error: OSStatus) -> KeychainError {
+        switch error {
+        case errSecItemNotFound:
+            return .itemNotFound
+        case errSecDataTooLarge:
+            return .invalidData
+        case errSecDuplicateItem:
+            return .duplicateItem
+        default:
+            return .unexpected(error)
+        }
+    }
+}
+
+// MARK: - Dictionary
+
+extension KeychainManager.KeychainDictionary {
+    mutating func addAttributes(_ attributes: KeychainManager.ItemAttributes) {
+        for(key, value) in attributes {
+            self[key as String] = value
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,89 @@
+## Description
+
+Swifty keychain wrapper.
+
+## Usage
+
+Copy the KeychainManager.swift file into your project.
+
+For every item that you want to save, update, delete or retrieve you must provide a list of attributes in order to identify the item correctly. Depending on the item class you chose, the available attributes you're going to have available for usage. You can check the full list [here](https://developer.apple.com/documentation/security/keychain_services/keychain_items/item_attribute_keys_and_values).
+
+### Items types
+
+More information, [here](https://developer.apple.com/documentation/security/keychain_services/keychain_items/item_class_keys_and_values#1678477).
+
+- **Generic**
+- **Password**
+- **Certificate**
+- **Cryptography**
+- **Identity**
+
+
+### Operations
+
+**Save item**
+
+```swift
+let apiToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb2Fsc2J1ZGR5IiwiZXhwIjo2NDA5MjIxMTIwMH0.JoDuSMARI2Ihh8fisiUxfQiP8AE_WFz9Hcogkk8QMcQ"
+do {
+    let apiTokenAttributes: KeychainManager.ItemAttributes = [
+        kSecAttrLabel: "ApiToken"
+    ]
+    try KeychainManager.shared.saveItem(apiToken, itemClass: .generic, attributes: apiTokenAttributes)
+} catch let keychainError as KeychainManager.KeychainError {
+    print(keychainError.localizedDescription)
+} catch {
+    print(error)
+}
+```
+
+**Update item**
+
+```swift
+do {
+    let apiTokenAttributes: KeychainManager.ItemAttributes = [
+        kSecAttrLabel: "ApiToken"
+    ]
+    
+    try KeychainManager.shared.updateItem(with: "new-token-value", ofClass: .generic, attributes: apiTokenAttributes)
+    
+} catch let keychainError as KeychainManager.KeychainError {
+    print(keychainError.localizedDescription)
+} catch {
+    print(error)
+}
+```
+
+**Delete item**
+
+```swift
+do {
+    let apiTokenAttributes: KeychainManager.ItemAttributes = [
+        kSecAttrLabel: "ApiToken"
+    ]
+    
+    try KeychainManager.shared.deleteImte(ofClass: .generic, attributes: apiTokenAttributes)
+    
+} catch let keychainError as KeychainManager.KeychainError {
+    print(keychainError.localizedDescription)
+} catch {
+    print(error)
+}
+```
+
+**Retrieve item**
+
+```swift
+do {
+    let apiTokenAttributes: KeychainManager.ItemAttributes = [
+        kSecAttrLabel: "ApiToken"
+    ]
+    
+    let token: String = try KeychainManager.shared.retrieveItem(ofClass: .generic, attributes: apiTokenAttributes)
+    
+} catch let keychainError as KeychainManager.KeychainError {
+    print(keychainError.localizedDescription)
+} catch {
+    print(error)
+}
+```
